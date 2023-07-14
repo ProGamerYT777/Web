d84ada7aa2c5afbbf2175dcab1e406f84a569bae
Мирослав <mir.ram05@yandex.ru>
2023-07-14 22:37:46 +0400

    HW

--- src/main/java/ru/skypro/lessons/springboot/web/repository/EmployeeRepository.java
+++ src/main/java/ru/skypro/lessons/springboot/web/repository/EmployeeRepository.java
@@ -10,13 +10,15 @@ import ru.skypro.lessons.springboot.web.model.Position;
 import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
+    @Query (value = "SELECT * " +
+            "FROM Employee e " +
+            "WHERE salary = (SELECT MAX(salary) FROM Employee e2)",
+             nativeQuery = true)
     List<Employee> getEmployeeWithHighestSalary();
     List<Employee> getEmployeesByPositionLike(Position position);
 
     @Query("SELECT e.name , e.salary , p.name " +
             "FROM Employee e join fetch Position p " +
-            "WHERE id = ?1 and e.position = p")
+            "WHERE e.id = ?1 and e.position = p")
     List<EmployeeFullInfo> getFullInfo(Integer id);
-    List<Employee> getPageInfo(int pageIndex, int unitPerPage);
 }
