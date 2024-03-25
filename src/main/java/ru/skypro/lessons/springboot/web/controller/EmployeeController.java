875ab42990c2334cbd044fc3c2c954ac5f1d1f06
Мирослав <mir.ram05@yandex.ru>
2024-03-26 01:27:08 +0400

    HW

--- src/main/java/ru/skypro/lessons/springboot/web/controller/EmployeeController.java
+++ src/main/java/ru/skypro/lessons/springboot/web/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
